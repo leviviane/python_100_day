cf778d52911c084dd90421b976430c760aa33a45
Viviane Le <letranviviane@gmail.com>
2024-06-18 16:42:44 -0700

    day 1 challenge

---
+++ day_1/index.py
@@ -0,0 +1,9 @@
+#1. create a greeting for your program
+
+#2. Ask the user for the city that they grew up in
+
+#3. Ask the user for the name of their pet
+
+#4. Combine the name of their city and pet and show them their band name.
+
+#4. Make sure the input cursor shows on a new line:
